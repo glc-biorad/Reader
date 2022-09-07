52d6c9d262d309e98e005730733f4a20be6623e5
Gabe Lopez-Candales <107704834+glc-biorad@users.noreply.github.com>
2022-09-07 10:06:09 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,55 +1,47 @@
 '''
 DESCRIPTION:
-This module contains the Controller which deals with read/writing to the COM port and executing commands. 
-
-USAGE:
-python controller.py -COM COM_port -i input_filename -o output_filename
-
-NOTES:
-Controller is inherets from serial.Serial
-
-All command line arguments are optional, the default arguments are defined
-at the top of this file. The input commands are assumed
-to be separated by a newline character (one command per line). The output is
-(command), (response)
-..., ...
-
-This file should read a csv file to get a list of serial commands, store those
-in the approriate data structure, write the commands in an arbitrary order to
-a specifiable COM port (default /dev/ttyUSB0), record the response in a separate
-csv file, preferably also with the command that produced the response
 '''
 
-import sys
-import time
-import serial
+import clr
 
-from coordinate import Coordinate
-from parse import Parser
+clr.AddReference('System.IO')
 
-from commands import commands
+from System.IO import Ports
 
-class Controller(serial.Serial):
+class Controller():
     # Public variables.
 
     # Private variables.
-    __COM_port = None
-    __baud_rate = None
-    __parser = Parser()
-    __command_list = None
-    __ADDRESS_MOTOR_X = 0x01
-    __ADDRESS_MOTOR_Y = 0x02
-    __ADDRESS_MOTOR_Z = 0x03
-    __VELOCITY = Coordinate([100000, 100000, 100000])
 
+    # Private constants
+    __SERIAL_PORT = None
+    __COM_PORT = 'COM8'
+    __BAUD_RATE = 115200
+    __TIMEOUT_READLINE = 1000
+    __TIMEOUT_WRITELINE = 1000
+    __PARITY = None
+    __HANDSHAKE = None
+    __DATABITS = 8
+    __STOPBITS = 'One'
+    __NEWLINE = '\r'
+
+    # Constructor.
     def __init__(self):
-        # Get the COM port and baud rate.
-        self.__COM_port = self.__parser.getCOMPort()
-        self.__baud_rate = self.__parser.getBaudRate()
+        self.__SERIAL_PORT = Ports.SerialPort(PortName=self.__COM_PORT, BaudRate=self.__BAUD_RATE)
+        self.__SERIAL_PORT.ReadTimeout = self.__TIMEOUT_READLINE
+        self.__SERIAL_PORT.WriteTimeout = self.__TIMEOUT_WRITELINE
+        self.__SERIAL_PORT.NewLine = self.__NEWLINE
+        self.__SERIAL_PORT.Open()
 
-        # Setup the serial connection to the COM port.
-        try:
-            super().__init__(self.__COM_port, self.__baud_rate, timeout=2)
-        except Exception as e:
-            print(e)
-            print("Connection timeout on COM port {0}".format(self.__COM_port))
+    def write(self, command):
+        self.__SERIAL_PORT.WriteLine(command.decode())
+
+    def readline(self):
+        response = self.__SERIAL_PORT.ReadLine()
+        return response
+
+    def close(self):
+        self.__SERIAL_PORT.Close()
+
+    def test(self):
+        print("HERE")
